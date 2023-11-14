f0eff8011f9711d89ff62e05f0fa2ad4b6b96329
KauanMO <kauaanmatheus@gmail.com>
2023-11-14 20:16:52 -0300

    correcao docker.sh

--- docker.sh
+++ docker.sh
@@ -1,28 +1,30 @@
-echo "Instalando o docker: "
+#!/bin/sh
+# Mensagem informativa sobre a instalação do Docker
+echo "Instalando o Docker..."
 sudo apt install docker.io
-
-echo "Docker instalado."
-
-echo "Iniciando docker: "
+echo "Docker instalado com sucesso!"
+# Iniciar o serviço do Docker
+echo "Iniciando o serviço do Docker..."
 sudo systemctl start docker
 sudo systemctl enable docker
-
-echo "Baixando a imagem MySQL: "
+# Baixar a imagem do MySQL 5.7
+echo "Baixando a imagem do MySQL 5.7..."
 sudo docker pull mysql:5.7
-echo "Imagem do MySQL 5.7 baixada."
-
-echo "Criando e executando o container MySQL: "
-sudo docker run -d -p 3306:3306 --name magister -e "MYSQL_ROOT_PASSWORD=aluno" mysql:5.7
-echo "Container MySQL criado."
-
+echo "Imagem do MySQL 5.7 baixada com sucesso!"
+# Criar e executar o container MySQL
+echo "Criando e executando o container MySQL..."
+sudo docker run -d -p 3306:3306 --name ByteGuard -e "MYSQL_ROOT_PASSWORD=aluno" mysql:5.7
+echo "Container MySQL criado e em execução!"
+# Executar o script SQL dentro do container MySQL
 echo "Executando o script SQL dentro do container MySQL..."
 sleep 15
-sudo docker exec -i magister mysql -u root -paluno < /home/ubuntu/docker-scripts-repo/script.sql
-
-echo "Script SQL executado."
-
-echo "Dando permissão de execução ao arquivo java.sh: 
+sudo docker exec -i ByteGuard mysql -u root -paluno < /home/ubuntu/docker-scripts-repo/script.sql
+echo "Script SQL executado com sucesso!"
+# Dar permissão de execução ao arquivo java.sh
+echo "Dando permissão de execução ao arquivo java.sh..."
 chmod +x java.sh
-echo "Permissão concedida."
-
-./java.sh
+echo "Permissão concedida com sucesso!"
+# Executar o arquivo java.sh
+echo "Executando o arquivo install-java.sh..."
+./install-java.sh
+echo "Arquivo install-java.sh executado com sucesso!"
