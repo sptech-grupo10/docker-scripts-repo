62097aa82cae3cb2911a245e4a9c74d36390940e
Kauan Oliveira <70850934+KauanMO@users.noreply.github.com>
2023-11-17 20:09:49 -0300

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-
-# URL do arquivo JAR no GitHub
 jar_url="https://github.com/sptech-grupo10/Java/raw/main/target/jar-byteguard-1.0-SNAPSHOT-jar-with-dependencies.jar"
 
-# Nome do arquivo JAR após o download
 jar_nome="jar-byteguard-1.0-SNAPSHOT-jar-with-dependencies.jar"
 
 echo "Agora iremos verificar se você já possui o Java instalado, aguarde um instante..."
@@ -39,31 +35,26 @@ else
     echo "Você já possui o Java instalado!"
 fi
 
-# Verificar se o arquivo JAR já existe
 if [ ! -f "$jar_nome" ]; then
-    echo "Baixando o arquivo JAR..."
-    # Instale o wget se não estiver instalado
+    echo "Baixando o arquivo JAR"
+    
     sudo apt install wget -y
-    # Baixar o arquivo JAR usando wget
     wget "$jar_url" -O "$jar_nome"
 
-    # Verificar se o download foi bem-sucedido
     if [ $? -eq 0 ]; then
-        echo "Download do arquivo JAR concluído com sucesso."
+        echo "Download do arquivo JAR concluído com sucesso"
     else
-        echo "Erro ao baixar o arquivo JAR."
+        echo "Erro ao baixar o arquivo JAR"
         exit 1
     fi
 else
-    echo "Arquivo JAR já existe. Pulando o download."
+    echo "Arquivo JAR já existe. Pulando o download"
 fi
 
-# Executar o arquivo JAR
 java -jar "$jar_nome"
 
-# Verificar se a execução foi bem-sucedida
 if [ $? -eq 0 ]; then
-    echo "Execução do arquivo JAR bem-sucedida."
+    echo "Execução do arquivo JAR bem-sucedida"
 else
-    echo "Erro ao executar o arquivo JAR."
+    echo "Erro ao executar o arquivo JAR"
 fi
